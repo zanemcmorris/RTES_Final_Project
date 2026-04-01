75044e041e8a53610b766ed7721fdbdf89b6273f
zanemcmorris <95657127+zanemcmorris@users.noreply.github.com>
2026-04-01 14:19:13 -0600

    adding helloworld

---
+++ F401_HelloWorld/Core/Src/main.c
@@ -0,0 +1,402 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "lsm6dsr.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI2_Init(void);
+void MX_PA8_CS_Init(void);
+static int32_t MX_LSM6DSR_Init(void);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+#define IMU_CS_PORT GPIOA
+#define IMU_CS_PIN  GPIO_PIN_8
+
+static LSM6DSR_Object_t MotionSensor;
+
+static void IMU_CS_Low(void)
+{
+	HAL_GPIO_WritePin(IMU_CS_PORT, IMU_CS_PIN, GPIO_PIN_RESET);
+}
+
+static void IMU_CS_High(void)
+{
+	HAL_GPIO_WritePin(IMU_CS_PORT, IMU_CS_PIN, GPIO_PIN_SET);
+}
+
+static int32_t BSP_SPI2_Init(void)
+{
+	IMU_CS_High();
+	return 0;
+}
+
+static int32_t BSP_SPI2_DeInit(void)
+{
+	IMU_CS_High();
+	return 0;
+}
+
+static int32_t BSP_GetTick(void)
+{
+	return (int32_t) HAL_GetTick();
+}
+
+static int32_t BSP_SPI2_WriteReg(uint16_t Addr, uint16_t Reg, uint8_t *pData,
+		uint16_t Length)
+{
+	(void) Addr;
+
+	uint8_t txbuf[32];
+
+	if (Length == 0 || Length > (sizeof(txbuf) - 1))
+		return LSM6DSR_ERROR;
+
+	txbuf[0] = (uint8_t) Reg;
+	if (Length > 1U)
+		txbuf[0] |= 0x40;   // auto-increment
+
+	memcpy(&txbuf[1], pData, Length);
+
+	IMU_CS_Low();
+
+	if (HAL_SPI_Transmit(&hspi2, txbuf, Length + 1, 100) != HAL_OK)
+	{
+		IMU_CS_High();
+		return LSM6DSR_ERROR;
+	}
+
+	IMU_CS_High();
+	return LSM6DSR_OK;
+}
+
+static int32_t BSP_SPI2_ReadReg(uint16_t Addr, uint16_t Reg, uint8_t *pData,
+		uint16_t Length)
+{
+	(void) Addr;
+
+	uint8_t txbuf[32];
+	uint8_t rxbuf[32];
+
+	if (Length == 0 || Length > (sizeof(txbuf) - 1))
+		return LSM6DSR_ERROR;
+
+	txbuf[0] = (uint8_t) Reg | 0x80;   // read bit
+	if (Length > 1U)
+		txbuf[0] |= 0x40;             // auto-increment
+
+	memset(&txbuf[1], 0x00, Length);
+	memset(rxbuf, 0x00, Length + 1);
+
+	IMU_CS_Low();
+
+	if (HAL_SPI_TransmitReceive(&hspi2, txbuf, rxbuf, Length + 1, 100) != HAL_OK)
+	{
+		IMU_CS_High();
+		return LSM6DSR_ERROR;
+	}
+
+	IMU_CS_High();
+
+	memcpy(pData, &rxbuf[1], Length);
+	return LSM6DSR_OK;
+}
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
+	MX_PA8_CS_Init();
+	MX_SPI2_Init();
+
+//	while (1)
+//	{
+//		uint8_t tx[2] = {0x8F, 0x00};
+//		uint8_t rx[2] = {0};
+//
+//		IMU_CS_Low();
+//		HAL_SPI_TransmitReceive(&hspi2, tx, rx, 2, 100);
+//		IMU_CS_High();
+//
+//		HAL_Delay(10);
+//	}
+
+
+	if (MX_LSM6DSR_Init() != LSM6DSR_OK)
+	{
+		while (1)
+		{
+		}
+	}
+
+	while (1)
+	{
+		LSM6DSR_Axes_t accel;
+		LSM6DSR_Axes_t gyro;
+
+		if (LSM6DSR_ACC_GetAxes(&MotionSensor, &accel) == LSM6DSR_OK)
+		{
+			// accel.x accel.y accel.z
+		}
+
+		if (LSM6DSR_GYRO_GetAxes(&MotionSensor, &gyro) == LSM6DSR_OK)
+		{
+			// gyro.x gyro.y gyro.z
+		}
+
+		HAL_Delay(20);
+	}
+}
+
+static int32_t MX_LSM6DSR_Init(void)
+{
+	LSM6DSR_IO_t io_ctx;
+	uint8_t id = 0;
+	int32_t fullScale = 2;
+
+	io_ctx.Init = BSP_SPI2_Init;
+	io_ctx.DeInit = BSP_SPI2_DeInit;
+	io_ctx.BusType = LSM6DSR_SPI_4WIRES_BUS;
+	io_ctx.Address = 0;
+	io_ctx.WriteReg = BSP_SPI2_WriteReg;
+	io_ctx.ReadReg = BSP_SPI2_ReadReg;
+	io_ctx.GetTick = BSP_GetTick;
+
+	if (LSM6DSR_RegisterBusIO(&MotionSensor, &io_ctx) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_Init(&MotionSensor) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_ReadID(&MotionSensor, &id) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	/* Optional sanity check: expected WHO_AM_I is 0x6B */
+	if (id != 0x6B)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_ACC_Enable(&MotionSensor) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_GYRO_Enable(&MotionSensor) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_ACC_SetOutputDataRate(&MotionSensor, 104.0f) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_GYRO_SetOutputDataRate(&MotionSensor, 104.0f) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	if (LSM6DSR_ACC_SetFullScale(&MotionSensor, fullScale) != LSM6DSR_OK)
+		return LSM6DSR_ERROR;
+
+	return LSM6DSR_OK;
+}
+
+void MX_PA8_CS_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	/* Enable GPIOA clock */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	/* Set CS high before fully configuring, so the sensor stays deselected */
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+
+	/* Configure PA8 as output push-pull */
+	GPIO_InitStruct.Pin = GPIO_PIN_8;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI2_Init(void)
+{
+
+	/* USER CODE BEGIN SPI2_Init 0 */
+
+	/* USER CODE END SPI2_Init 0 */
+
+	/* USER CODE BEGIN SPI2_Init 1 */
+
+	/* USER CODE END SPI2_Init 1 */
+	/* SPI2 parameter configuration*/
+	hspi2.Instance = SPI2;
+	hspi2.Init.Mode = SPI_MODE_MASTER;
+	hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+	hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+	hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+	hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi2.Init.NSS = SPI_NSS_SOFT;
+	hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+	hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	hspi2.Init.CRCPolynomial = 10;
+	if (HAL_SPI_Init(&hspi2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN SPI2_Init 2 */
+
+	/* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ F401_HelloWorld/Debug/Core/Src/lsm6dsr.d
@@ -0,0 +1,4 @@
+Core/Src/lsm6dsr.o: ../Core/Src/lsm6dsr.c ../Core/Inc/lsm6dsr.h \
+ ../Core/Inc/lsm6dsr_reg.h
+../Core/Inc/lsm6dsr.h:
+../Core/Inc/lsm6dsr_reg.h:
